a21f87be078fc1c804bec5d61e53f06bf8be8778
iman abdolahi <abdolahi.iman73@gmail.com>
2023-05-23 03:09:43 +0430

    Add SecurityController

---
+++ Endpoint.Api/Controllers/SecurityController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Endpoint.Api.Controllers
+{
+    [Route("&quotapi /[controller] & quot")]
+    [ApiController]
+    [Authorize]
+    public class SecurityController : Controller
+    {
+        [HttpGet("&quotgetFruits & quot")]
+        [AllowAnonymous]
+        public ActionResult GetFruits()
+        {
+            List<string> mylist = new List<string>() { "&quotapples & quot, &quotbannanas & quot" };
+            return Ok(mylist);
+        }
+        [HttpGet("&quotgetFruitsAuthenticated & quot")]
+        public ActionResult GetFruitsAuthenticated()
+        {
+            List<string> mylist = new List<string>() { "&quotorganic apples & quot, &quotorganic bannanas & quot" };
+            return Ok(mylist);
+        }
+    }
+}
--- Endpoint.Api/Program.cs
+++ Endpoint.Api/Program.cs
@@ -1,6 +1,37 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var key = Encoding.ASCII.GetBytes("&quot@#MY_BIG_SECRET_KEY@#&quot");
 // Add services to the container.
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; ;
+}).AddJwtBearer(options =>
+{
+    options.Events = new JwtBearerEvents
+    {
+        OnTokenValidated = context =>
+        {
+            //TODO CODE
+            return Task.CompletedTask;
+        }
+    };
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +46,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
